57bdca4cc08d7cc0eed556ed77118b72cdc49d4f
IgorPushka <igor081991@yandex.ru>
2024-12-08 14:08:45 +0300

    fix import

--- src/test/java/ru/yandex/masterskaya/service/LikeServiceImplTest.java
+++ src/test/java/ru/yandex/masterskaya/service/LikeServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.masterskaya.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.masterskaya.client.EventClient;
 import ru.yandex.masterskaya.client.RegistrationClient;
